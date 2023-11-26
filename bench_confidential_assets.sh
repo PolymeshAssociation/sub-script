f5dfeb1d0a6b24087138ed5bdc3cfcebbe4b3ee5
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2023-11-27 00:32:49 +0800

    Fix settlement benchmarks for multi-node networks.

--- bench_confidential_assets.sh
+++ bench_confidential_assets.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
 URL=ws://localhost:9944
+#URL=ws://comp002:9944
 
 time -p ./target/release/run -u $URL ./scripts/bench_confidential_assets_01_setup.rhai $*
 time -p ./target/release/run -u $URL ./scripts/bench_confidential_assets_02_fund.rhai $*
+sleep 12
 time -p ./target/release/run -u $URL ./scripts/bench_confidential_assets_03_orders.rhai $*
--- bench_settlement.sh
+++ bench_settlement.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
 URL=ws://localhost:9944
+#URL=ws://comp002:9944
 
 time -p ./target/release/run -u $URL ./scripts/bench_settlement_01_setup.rhai $*
 time -p ./target/release/run -u $URL ./scripts/bench_settlement_02_fund.rhai $*
+sleep 12
 time -p ./target/release/run -u $URL ./scripts/bench_settlement_03_orders.rhai $*
