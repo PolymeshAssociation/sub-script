3db03975dd73bba6aacbfa2575737a5855637d6a
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2024-08-27 21:12:36 +0800

    Add non-confidential asset portfolio move benchmark.

---
+++ bench_asset_moves.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+#URL=ws://localhost:9944
+URL=ws://comp002:9944
+
+time -p ./target/release/run -u $URL ./scripts/bench_asset_moves_01_setup.rhai $*
+time -p ./target/release/run -u $URL ./scripts/bench_asset_moves_02_fund.rhai $*
+sleep 12
+time -p ./target/release/run -u $URL ./scripts/bench_asset_moves_02_fund.rhai $*
