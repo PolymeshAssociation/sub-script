3adae1ce74f2df82f0f8b279839395ec4a178204
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2024-08-29 22:36:29 +0800

    change node URL in bench script.

--- bench_asset_moves.sh
+++ bench_asset_moves.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-#URL=ws://localhost:9944
-URL=ws://comp002:9944
+URL=ws://localhost:9944
+#URL=ws://comp002:9944
 
 time -p ./target/release/run -u $URL ./scripts/bench_asset_moves_01_setup.rhai $*
 time -p ./target/release/run -u $URL ./scripts/bench_asset_moves_02_fund.rhai $*
